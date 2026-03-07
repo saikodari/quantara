8bebdb02f43676df8c319eab6c89b635c77b8ad5
Saikumar kodari <saikdoari@gmail.com>
2026-03-07 16:39:34 -0500

    Switch project to .NET 8 and remove OpenAPI

--- src/api/Quantara.API/Program.cs
+++ src/api/Quantara.API/Program.cs
@@ -2,15 +2,15 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+//builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.MapOpenApi();
+//}
 
 app.UseHttpsRedirection();
 
